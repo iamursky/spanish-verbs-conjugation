4b266da0333846ff4ab7d596653de59c2787ba25
Ilya Amursky <ilya.amursky@gmail.com>
2023-05-30 22:05:52 +0200

    chore: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="w-screen h-screen flex justify-center items-center">
+      Spanish Verbs Conjugation
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // prettier-ignore
+    "./app/**/*.tsx",
+    "./components/**/*.jsx",
+  ],
+};
